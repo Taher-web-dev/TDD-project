26de97e407a736bfceee51c495fe7e6bae9904f5
Taher <ttaherhagui@gmail.com>
2022-02-03 16:26:52 +0100

    create factorial function to pass its tests.

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,2 +1,11 @@
 class Solver
+  def factorial(number)
+    return 1 if number == 0
+    raise TypeError.new("Factorial of negative integer") unless number >= 0
+    res = 1
+    (1..number).each do |n|
+      res *= n
+    end
+    res
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,10 +3,10 @@ describe Solver do
   context "When testing factorial method" do
     solver = Solver.new
     it "return 0 when it receive 0" do
-      expect(solver.factorial(0)).to be 0
+      expect(solver.factorial(0)).to be 1
     end
     it "Raise exception when it receives negative integer" do
-      expect(solver.factorial(-1)).to raise_error("Factorial of negative integer")
+      expect{solver.factorial(-1)}.to raise_error("Factorial of negative integer")
     end
     it "return factorial number for a given positive integer" do
       expect(solver.factorial(3)).to be 6
